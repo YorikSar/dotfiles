dd373d436b79eb2f9c095db2389da367364c7edc
Yuriy Taraday <yorik.sar@gmail.com>
2020-11-23 00:45:31 +0400

    Configure niv, pin it and home-manager in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,15 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+let
+  sources = import ./nix/sources.nix;
+in
+{ 
+  pkgs ? import sources.nixpkgs {}
+}:
+let
+  home-manager = import sources.home-manager { inherit pkgs; };
+in
 pkgs.mkShell {
   buildInputs = [
+    pkgs.niv
+    home-manager.home-manager
   ];
 }
