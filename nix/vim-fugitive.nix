7b8bd78dda1f79e6eab9a717085b663e076459fc
Yuriy Taraday <yorik.sar@gmail.com>
2021-10-03 17:49:33 +0400

    Revert "Test my nix-fugitive patch to address tpope/vim-fugitive#1509"
    This reverts commit 97b50f8b13acc13f1c35b0e1b3cda88caa850d42.
    
    The fix has been applied to vim-fugitive proper.

--- nix/vim-fugitive.nix
+++ nix/vim-fugitive.nix
@@ -1,19 +1,8 @@
 { config, pkgs, ... }:
 
-let
-
-  vim-fugitive = pkgs.vimPlugins.vim-fugitive.overrideAttrs (old: {
-    patches = [
-      (pkgs.fetchpatch {
-        url = "https://github.com/YorikSar/vim-fugitive/commit/d510c6a27a9bc63e30f3116a2a3817871bd25ef5.patch";
-        sha256 = "0pb07qf4mr5iv2w8chqn42p9x1is0g77gvdw5h053n58h7h6wwy6";
-      })
-    ];
-  });
-in
 {
   programs.neovim = {
-    plugins = [
+    plugins = with pkgs.vimPlugins; [
       vim-fugitive
     ];
     extraConfig = ''
