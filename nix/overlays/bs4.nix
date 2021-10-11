8a701728ebf10ced069a282b9484090d35cb2ea8
Yuriy Taraday <yorik.sar@gmail.com>
2021-10-12 01:22:12 +0400

    Add beautifulsoup4 overlay
    Until NixOS/nixpkgs#137870 fixing NixOS/nixpkgs#137678 lands
    in nixpkgs-unstable.

---
+++ nix/overlays/bs4.nix
@@ -0,0 +1,14 @@
+self: super:
+let
+  lib = super.lib;
+in
+rec {
+  python39 = super.python39.override {
+    packageOverrides = self: super: {
+      beautifulsoup4 = super.beautifulsoup4.overrideAttrs (old: {
+        propagatedBuildInputs = lib.remove super.lxml old.propagatedBuildInputs;
+      });
+    };
+  };
+  python39Packages = python39.pkgs;
+}
