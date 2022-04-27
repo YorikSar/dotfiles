5497c587b784b24dec91e3df8b3a4a3944ca9413
Yuriy Taraday <yorik.sar@gmail.com>
2022-04-27 18:10:32 +0400

    Bump nixpkgs-unstable to latest
    Fix golangci-lint according to recommendation in comment
    https://github.com/NixOS/nixpkgs/issues/168984#issuecomment-1109119619

--- nix/go.nix
+++ nix/go.nix
@@ -1,4 +1,12 @@
 { config, pkgs, ... }:
+let
+  # From https://github.com/NixOS/nixpkgs/issues/168984#issuecomment-1109119619
+  golangci-lint-117 = pkgs.golangci-lint.override ({
+    # Override https://github.com/NixOS/nixpkgs/pull/166801 which changed this
+    # to buildGo118Module because it does not build on Darwin.
+    buildGoModule = pkgs.buildGoModule;
+  });
+in
 
 {
   programs.go = {
@@ -14,7 +22,7 @@
     gotools
     gopls
     dep
-    golangci-lint
-    clang  # looks like gopls needs this
+    golangci-lint-117
+    clang # looks like gopls needs this
   ];
 }
