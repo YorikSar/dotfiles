d7eccf447a399c15fe987ab02db13f4ef1e1b557
Yuriy Taraday <yorik.sar@gmail.com>
2023-07-18 23:17:39 +0200

    Copy apps to ~/Applications instead of symlinking them
    Mark the source of app bundle in xattr to not copy them over and over.
    Adapted from https://github.com/nix-community/home-manager/issues/1341#issuecomment-881709503
    see issue for more details.

--- nix/profiles/macos.nix
+++ nix/profiles/macos.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ../xcrun-subst.nix
     ../alacritty.nix
@@ -9,4 +14,43 @@
   programs.ssh.extraOptionOverrides = {
     UseKeychain = "yes";
   };
+
+  # Replace default symlinking functionality with copying
+  home.file."Applications/Home Manager Apps".enable = false;
+  home.activation.copyApps = let
+    apps = pkgs.buildEnv {
+      name = "home-manager-applications";
+      paths = config.home.packages;
+      pathsToLink = "/Applications";
+    };
+    v = "\${VERBOSE_ARG:+-v}";
+  in
+    lib.hm.dag.entryAfter ["linkGeneration"] ''
+      baseDir="$HOME/Applications/Home Manager Apps"
+      $DRY_RUN_CMD mkdir -p "$baseDir"
+      shopt -s nullglob
+      for app in "$baseDir"/*; do
+        if [ ! -e "${apps}/Applications/$(basename "$app")" ]; then
+          $DRY_RUN_CMD rm -r ${v} "$app"
+        fi
+      done
+      for app in ${apps}/Applications/*; do
+        source="$(readlink "$app")"
+        target="$baseDir/$(basename "$app")"
+        if [ -e "$target" ]; then
+          oldsource="$(xattr -p home-manager-source "$target" || true)"
+          if [ "$source" = "$oldsource" ]; then
+            continue
+          fi
+        fi
+        temp="$(mktemp -u "$target.XXXXXX")"
+        $DRY_RUN_CMD cp ${v} -HLR "$source" "$temp"
+        $DRY_RUN_CMD chmod ${v} -R +w "$temp"
+        $DRY_RUN_CMD xattr -w home-manager-source "$source" "$temp"
+        if [ -e "$target" ]; then
+          $DRY_RUN_CMD rm -r ${v} "$target"
+        fi
+        $DRY_RUN_CMD mv ${v} "$temp" "$target"
+      done
+    '';
 }
