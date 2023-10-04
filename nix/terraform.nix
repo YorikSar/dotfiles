95bd2db971826d134578fece9d30b434217f7338
Yuriy Taraday <yuriy.taraday@tweag.io>
2023-10-04 16:57:30 +0200

    Fix terraform filetype handling and language server

--- nix/terraform.nix
+++ nix/terraform.nix
@@ -1,13 +1,16 @@
 {pkgs, ...}: {
   programs.neovim = {
     extraConfig = ''
-      let g:LanguageClient_serverCommands['hcl'] = ['terraform-ls']
+      let g:LanguageClient_serverCommands['hcl'] = ['terraform-ls','serve']
+      let g:LanguageClient_serverCommands['terraform'] = ['terraform-ls','serve']
+      let g:LanguageClient_serverCommands['terraform-vars'] = ['terraform-ls','serve']
     '';
     plugins = with pkgs.vimPlugins; [
-      (nvim-treesitter.withPlugins (p: [p.hcl]))
+      (nvim-treesitter.withPlugins (p: [p.hcl p.terraform]))
     ];
   };
   home.packages = with pkgs; [
+    terraform
     terraform-ls
   ];
 }
