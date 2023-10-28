aca643fe18762342f34c2bb0ae7485c64fdb345e
Yuriy Taraday <yorik.sar@gmail.com>
2023-10-28 16:26:06 +0200

    Add Touch ID support for sudo

--- nix/darwin/leya.nix
+++ nix/darwin/leya.nix
@@ -20,4 +20,8 @@
     };
     package = pkgs.nixUnstable;
   };
+  environment.etc."pam.d/sudo_local".text = ''
+    auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so
+    auth       sufficient     pam_tid.so
+  '';
 }
