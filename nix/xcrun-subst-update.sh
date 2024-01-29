054706b2dd7bee35bf4b00074fadd1d6f7bb41dd
Yuriy Taraday <yuriy.taraday@tweag.io>
2024-01-29 12:32:05 +0100

    xcrun-subst: Be smarter about selecting binaries
    Include the second category of binaries not equal to /usr/bin/git
    and xcrun.

--- nix/xcrun-subst-update.sh
+++ nix/xcrun-subst-update.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 d="$(dirname "$0")"
-binCache="$(md5 -q /usr/bin/git)"
-find {,/usr}/{,s}bin -type f | xargs md5 -r | grep "$binCache" | cut -f 4 -d / | sort -u > "$d/xcrun-subst.lst"
+otool="$(nix build --no-link --print-out-paths nixpkgs\#darwin.binutils-unwrapped^out)/bin/otool"
+
+# Binaries that are shims for xcode-select have only following deps:
+# $ otool -L /usr/bin/git
+# /usr/bin/git (architecture x86_64):
+#         /usr/lib/libxcselect.dylib (compatibility version 1.0.0, current version 1.0.0)
+#         /usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1336.80.1)
+# /usr/bin/git (architecture arm64e):
+#         /usr/lib/libxcselect.dylib (compatibility version 1.0.0, current version 1.0.0)
+#         /usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1336.80.1)
+# Filter them out based on the presence of libxcselect.dylib and number of lines
+
+find {,/usr}/{,s}bin -type f -exec sh -c 'l="$('"$otool"' -L {})"; [ "$(echo "$l" | wc -l)" -eq 6 ] && ( echo $l | grep -q /usr/lib/libxcselect.dylib )' \; -print | cut -f 4 -d / | sort -u > "$d/xcrun-subst.lst"
