ed2b6b2411c581641e6fce56fc7cc94c809fe22f
Yuriy Taraday <yuriy.taraday@tweag.io>
2025-01-02 16:02:36 +0100

    ghostty: Stop all the padding

--- nix/ghostty.nix
+++ nix/ghostty.nix
@@ -9,6 +9,10 @@
     settings = {
       font-family = "Hack";
       theme = "dark:Builtin Solarized Dark,light:Builtin Solarized Light";
+      window-padding-x = 0;
+      window-padding-y = 0;
+      window-padding-balance = true;
+      window-padding-color = "extend";
     };
   };
   home.packages = [
