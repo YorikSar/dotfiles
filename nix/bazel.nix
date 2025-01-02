4496c9cfe9790187a1997d95f1e9dcaa5f856a5e
Yuriy Taraday <yorik.sar@gmail.com>
2025-01-02 12:08:32 +0100

    Update flake inputs
    Remove Bazel for now, because it is broken on Darwin in nixpkgs.

--- nix/bazel.nix
+++ nix/bazel.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    bazel
+    #bazel
   ];
   home.file.".bazelrc".text = ''
     build --repo_env=BAZEL_CXXOPTS="-I${pkgs.libcxx.dev}/include/c++/v1"
