fe585c0f1a86c8c68e7ae5c4c721d351ce8206fb
Yuriy Taraday <yuriy.taraday@tweag.io>
2025-02-17 00:36:29 +0100

    Remove lorri

--- nix/nix.nix
+++ nix/nix.nix
@@ -21,26 +21,8 @@
   };
 
   home.packages = with pkgs; [
-    lorri
     nixVersions.latest
     nixos-rebuild
     nix-output-monitor
   ];
-
-  launchd = {
-    enable = pkgs.stdenv.hostPlatform.isDarwin;
-    agents.lorri = {
-      enable = true;
-      config = {
-        ProgramArguments = ["${pkgs.lorri}/bin/lorri" "daemon"];
-        StandardOutPath = "/var/tmp/lorri.log";
-        StandardErrorPath = "/var/tmp/lorri.log";
-        KeepAlive = true;
-        RunAtLoad = true;
-        EnvironmentVariables = {
-          PATH = "${pkgs.nixVersions.latest}/bin";
-        };
-      };
-    };
-  };
 }
